d749cf9cbef4cb27158f127d9e4ac4f82a446430
Charlie Wray <charlesmwray@gmail.com>
2017-01-28 12:50:12 -0800

    Removes filter. Adds mobile view.

--- src/Index.js
+++ src/Index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 // components
 import App from './components/App';
 
-
 window.React = React;
 
 render(<App />, document.getElementById('container'));
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,31 +7,26 @@ const MovieList = (props) => {
     const filter = props.filter;
     const movieItems = props.movies.map( (movie, i) => {
         return (
-            <div key={i}>
-            {filter}
-                { !movie.filter &&
-                    <li className="list-group-item movie-list-item">
-                        {
-                            movie.poster &&
-                            <div className="poster">
-                                <img src={movie.poster} />
-                                <div className="year">{movie.year}</div>
-                            </div>
-                        }
-                        <div className="metadata-wrapper">
-                            <a href={movie.link} className="title" target="_blank">
-                                {movie.title}
-                            </a>
-                            <div className="notes">{movie.notes}</div>
-                        </div>
-                        <div className="action-wrapper">
-                            <span className="badge">Rating: {movie.rating}</span>
-                            { movie.watched && <span className="badge watched">Watched</span> }
-                            <button className="remove button btn" onClick={() => { removeMovie(movie.dbId) }}>Remove</button>
-                        </div>
-                    </li>
+            <li className="list-group-item movie-list-item" key={i}>
+                {
+                    movie.poster &&
+                    <div className="poster">
+                        <img src={movie.poster} />
+                        <div className="year">{movie.year}</div>
+                    </div>
                 }
-            </div>
+                <div className="metadata-wrapper">
+                    <a href={movie.link} className="title" target="_blank">
+                        {movie.title}
+                    </a>
+                    <div className="notes">{movie.notes}</div>
+                </div>
+                <div className="action-wrapper">
+                    <span className="badge">Rating: {movie.rating}</span>
+                    { movie.watched && <span className="badge watched">Watched</span> }
+                    <button className="remove button btn" onClick={() => { removeMovie(movie.dbId) }}>Remove</button>
+                </div>
+            </li>
         )
     });
     return (
